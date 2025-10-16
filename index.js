a69df024d15abc4b6bb41f099e0c0e8f8f0dde36
Silvineey <lucreciasilvio@hotmail.com>
2025-10-16 10:21:12 -0300

    transições
    transições

--- index.js
+++ index.js
@@ -2,3 +2,32 @@ function botoes(){
     const menu=document.querySelector(".menu")
     menu.classList.toggle("ativo")
 }
+
+function ocultar(){
+    const conteiner=document.querySelector(".conteiner")
+    conteiner.classList.add("ocultar")
+
+    setTimeout(() => {
+        conteiner.style.display="none"
+    }, 1000);
+}
+
+function galeria(id){
+    const clicado=document.getElementById(id)
+    if(!clicado)return
+    clicado.style.display="flex"
+    void clicado.offsetHeight
+    clicado.style.opacity="1"
+    clicado.style.visibility="visible"
+
+    const galerias=document.querySelectorAll("section.conteiner")
+    galerias.forEach(sections=>{
+        if(sections!==clicado){
+            sections.style.opacity=0
+            sections.style.visibility="hidden"
+            setTimeout(() => {
+                sections.style.display="none"
+            }, 1000);
+        }
+    })
+}
